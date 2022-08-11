2991a270756f3297ef34d6f17eb6f7c6a18c7b7c
Craig Peacock <craigp@beyondlogic.org>
2022-08-11 15:06:19 +0930

    Empty project

---
+++ MCP19119_Buck.X/main.c
@@ -0,0 +1,20 @@
+/*
+ * Example code for the Microchip MCP19119 Digitally Enhanced Power Analog Buck 
+ * Controller
+ * 
+ */
+
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select bit (MCLR pin is MCLR function and weak internal pull-up is enabled)
+#pragma config CP = OFF         // Code Protection bit (Program memory is not code protected)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+
+void main(void)
+{
+    
+    
+    
+}
